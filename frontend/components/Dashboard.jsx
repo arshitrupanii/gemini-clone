6b1e7737d59171f0857aa1668216a001d43aa3be
Arshit rupani <125478383+arshitrupanii@users.noreply.github.com>
2025-06-08 09:19:07 +0530

    update

--- frontend/components/Dashboard.jsx
+++ frontend/components/Dashboard.jsx
@@ -1,3 +1,4 @@
+import Prompt_bar from "./Prompt_bar"
 
 const Dashboard = () => {
   return (
@@ -42,49 +43,14 @@ const Dashboard = () => {
 
       {/* middle div */}
       <div className="flex w-[78vw] h-[70vh]  justify-center items-center ">
-        <p class="text-3xl text-transparent bg-clip-text bg-gradient-to-r font-medium from-indigo-500 via-purple-500 to-rose-500">
+        <p className="text-3xl text-transparent bg-clip-text bg-gradient-to-r font-medium from-indigo-500 via-purple-500 to-rose-500">
           Hello, Arshit
         </p>
       </div>
 
 
       {/* prompt bar */}
-      <div className="flex items-center justify-center">
-        <div className="absolute bottom-15 w-[55vw] flex flex-col gap-3 rounded-2xl border border-gray-800 p-5">
-
-          {/* message bar */}
-          <div>
-            <input className="w-full p-1 focus:outline-0 " type="text" name="prompt" id="prompt" placeholder="Ask gemini" />
-          </div>
-
-          <div className="flex items-center justify-between opacity-60">
-            <div className="flex gap-3 items-center">
-              <span className="px-1 py-1 hover:bg-gray-700 cursor-pointer rounded-full">
-                <img className="w-5 h-5" src="https://img.icons8.com/?size=100&id=11153&format=png&color=EBEBEB" alt="" />
-              </span>
-
-              <span className="flex gap-2 items-center hover:bg-gray-700 cursor-pointer rounded-3xl px-2 py-1">
-                <img className="w-5 h-5" src="https://img.icons8.com/?size=100&id=Ngf5dT8xL0GZ&format=png&color=EBEBEB" alt="" />
-                <span className="text-[13px]">Deep Research</span>
-              </span>
-
-              <span className="flex gap-2 items-center hover:bg-gray-700 cursor-pointer rounded-3xl px-2 py-1">
-                <img className="w-5 h-5" src="https://img.icons8.com/?size=100&id=40314&format=png&color=EBEBEB" alt="" />
-                <span className="text-[13px]">Canvas</span>
-              </span>
-            </div>
-
-            <div>
-              <span className="hover:bg-gray-700 cursor-pointer flex px-1 py-1 rounded-full">
-                <img className="w-6 h-6" src="https://img.icons8.com/?size=100&id=8LR9OAENZPeS&format=png&color=EBEBEB" alt="" />
-              </span>
-            </div>
-
-          </div>
-
-
-        </div>
-      </div>
+     <Prompt_bar/>
 
     </div>
   )
